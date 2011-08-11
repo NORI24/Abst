db87e10ec412f61530f23e8ec6392af20a9e4bc3
admin <admin@ee56edd8-5187-8145-a53a-5b855f78f1fc>
2011-08-11 16:02:12 +0000

    git-svn-id: svn+ssh://arith-c.math.se.tmu.ac.jp/var/svn/repos/abst/trunk@6 ee56edd8-5187-8145-a53a-5b855f78f1fc

---
+++ src/test/test_fundamental.rb
@@ -0,0 +1,15 @@
+require 'test/unit'
+
+class fundamental < Test::Unit::TestCase
+	def test_gcd
+		test_cases = [
+			[3, 4, 1],
+			[14, 21, 7],
+		]
+
+		test_cases.each do |a, b, g|
+			assert_equal(g, gcd(a, b))
+		end
+	end
+
+end
