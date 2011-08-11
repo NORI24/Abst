7ee939bff6f3b1a9213494248b7f4d74d434f248
admin <admin@ee56edd8-5187-8145-a53a-5b855f78f1fc>
2011-08-11 17:25:10 +0000

    add a method 'binary_gcd'
    git-svn-id: svn+ssh://arith-c.math.se.tmu.ac.jp/var/svn/repos/abst/trunk@12 ee56edd8-5187-8145-a53a-5b855f78f1fc

--- src/fundamental.rb
+++ src/fundamental.rb
@@ -1,9 +1,50 @@
 # return gcd(a, b)
-# a and b are member of Euclidean domain
+# Param::  a and b are member of Euclidean domain
+# Return:: gcd of a and b
 def gcd(a, b)
 	until b.zero?
 		a, b = b, a % b
 	end
 
 	return a
+end
+
+# Binary GCD
+# Param::  non-negative integer a, b
+# Return:: gcd of a and b
+def binary_gcd(a, b)
+	a, b = b, a if a < b
+
+	return a if (0 == b)
+
+	# 1. Reduce size once
+	a, b = b, a % b
+
+	return a if (0 == b)
+
+	# 2. Compute powers of 2
+	k = 0
+	while (0 == a[k] and 0 == b[k])
+		k += 1
+	end
+	a >>= k
+	b >>= k
+
+	# 3. Remove initial power of 2
+	a >>= 1 while 0 == a[0]
+	b >>= 1 while 0 == b[0]
+
+	loop do
+		# 4. Subtract (Here a and b are both odd.)
+		t = (a - b) >> 1
+		return 2 ** k * a if 0 == t
+
+		t >>= 1 if 0 == t[0]
+
+		if 0 < t
+			a = t
+		else
+			b = -t
+		end
+	end
 end
--- src/test/test_fundamental.rb
+++ src/test/test_fundamental.rb
@@ -12,4 +12,16 @@ class TC_Fundamental < Test::Unit::TestCase
 			assert_equal(g, gcd(a, b))
 		end
 	end
+
+	def test_binary_gcd
+		test_cases = [
+			[3, 4, 1],
+			[14, 21, 7],
+			[1024, 32, 32],
+		]
+
+		test_cases.each do |a, b, g|
+			assert_equal(g, binary_gcd(a, b))
+		end
+	end
 end
