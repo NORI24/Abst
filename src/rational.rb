6560b8fcb35e940713e3089601be07614525a45c
miyamoto <miyamoto@ee56edd8-5187-8145-a53a-5b855f78f1fc>
2011-09-11 03:48:07 +0000

    add method 'Rational.to_ds'
    git-svn-id: svn+ssh://arith-c.math.se.tmu.ac.jp/var/svn/repos/abst/trunk@112 ee56edd8-5187-8145-a53a-5b855f78f1fc

--- src/rational.rb
+++ src/rational.rb
@@ -7,21 +7,24 @@ class Rational
 		return Rational(0, 1)
 	end
 
-	def to_decimal_s(base = 10)
+	# to repeating decimal string
+	def to_rds(base = 10)
 		raise ArgumentError, "invalid radix #{base}" if base < 2 or 36 < base
 
 		# initialize
 		quotient = []
 
-		n = numerator.to_s(base).split('').map {|i| i.to_i}
+		n = numerator.to_s(base).split('').map(&:to_i)
 		exp = 0
 		while 0 == n.last
 			n.pop
 			exp += 1
 		end
 		d = denominator
-		while 0 == d % base
-			d /= base
+		loop do
+			q, r = d.divmod(base)
+			break unless 0 == r
+			d = q
 			exp -= 1
 		end
 		t = n[0]
@@ -76,6 +79,82 @@ class Rational
 				rslt += '(' if t == i[1]
 			end
 			rslt +=  ")"
+		end
+		rslt +=  "e" + exp.to_s unless 0 == exp
+
+		return rslt
+	end
+
+	# to decimal string
+	def to_ds(length = 64, base = 10)
+		raise ArgumentError, "invalid radix #{base}" if base < 2 or 36 < base
+
+		# initialize
+		quotient = []
+
+		n = numerator.to_s(base).split('').map(&:to_i)
+		exp = 0
+		while 0 == n.last
+			n.pop
+			exp += 1
+		end
+		d = denominator
+		loop do
+			q, r = d.divmod(base)
+			break unless 0 == r
+			d = q
+			exp -= 1
+		end
+		t = n[0]
+
+		# division loop
+		i = 1
+		s = nil
+		loop do
+			q, r = t.divmod(d)
+			t = r * base + n[i].to_i
+
+			s = i - 1 if nil == s and q != 0
+			quotient.push([q, nil])
+
+			unless n[i]
+				if 0 == t
+					quotient.last[1] = 0
+					break
+				end
+				break if s and i - s == length
+				quotient.last[1] = t
+			end
+
+			i += 1
+		end
+
+		# format
+		rslt = "0."
+
+		zero_count = 0
+		while 0 == quotient[0][0]
+			zero_count += 1
+			if t == quotient.shift[1]
+				# rotate 0
+				while 0 == quotient[0][0]
+					quotient.push(quotient.shift)
+					zero_count += 1
+				end
+				break
+			end
+		end
+		exp += n.size - zero_count
+
+		convert = "0123456789abcdefghijklmnopqrstuvwxyz"
+		if 0 == quotient.last[1]
+			rslt += quotient.inject('') do |r, i|
+				r + convert[i[0]]
+			end
+		else
+			quotient.each do |i|
+				rslt += convert[i[0]]
+			end
 		end
 		rslt +=  "e" + exp.to_s unless 0 == exp
 
--- src/test/test_rational.rb
+++ src/test/test_rational.rb
@@ -2,18 +2,31 @@ require 'test/unit'
 require 'abst'
 
 class TC_Rational < Test::Unit::TestCase
-	def test_to_decimal_s
-		assert_equal("0.5", Rational(1, 2).to_decimal_s)
-		assert_equal("0.(3)", Rational(1, 3).to_decimal_s)
-		assert_equal("0.25", Rational(1, 4).to_decimal_s)
-		assert_equal("0.2", Rational(1, 5).to_decimal_s)
-		assert_equal("0.1(6)", Rational(1, 6).to_decimal_s)
-		assert_equal("0.(142857)", Rational(1, 7).to_decimal_s)
-		assert_equal("0.125", Rational(1, 8).to_decimal_s)
-		assert_equal("0.(1)", Rational(1, 9).to_decimal_s)
-		assert_equal("0.1", Rational(1, 10).to_decimal_s)
-		assert_equal("0.1005e3", Rational(201, 2).to_decimal_s(10))
+	def test_to_rds
+		assert_equal("0.5", Rational(1, 2).to_rds)
+		assert_equal("0.(3)", Rational(1, 3).to_rds)
+		assert_equal("0.25", Rational(1, 4).to_rds)
+		assert_equal("0.2", Rational(1, 5).to_rds)
+		assert_equal("0.1(6)", Rational(1, 6).to_rds)
+		assert_equal("0.(142857)", Rational(1, 7).to_rds)
+		assert_equal("0.125", Rational(1, 8).to_rds)
+		assert_equal("0.(1)", Rational(1, 9).to_rds)
+		assert_equal("0.1", Rational(1, 10).to_rds)
+		assert_equal("0.1005e3", Rational(201, 2).to_rds(10))
+		assert_equal("0.(10)", Rational(2, 3).to_rds(2))
+	end
 
-		assert_equal("0.(10)", Rational(2, 3).to_decimal_s(2))
+	def test_to_ds
+		assert_equal("0.5", Rational(1, 2).to_ds)
+		assert_equal("0." + "3" * 15, Rational(1, 3).to_ds(15))
+		assert_equal("0.25", Rational(1, 4).to_ds)
+		assert_equal("0.2", Rational(1, 5).to_ds)
+		assert_equal("0.1" + "6" * 31, Rational(1, 6).to_ds(32))
+		assert_equal("0." + "142857" * 10, Rational(1, 7).to_ds(60, 10))
+		assert_equal("0.125", Rational(1, 8).to_ds)
+		assert_equal("0.11111", Rational(1, 9).to_ds(5))
+		assert_equal("0.1", Rational(1, 10).to_ds)
+		assert_equal("0.1005e3", Rational(201, 2).to_ds(10))
+		assert_equal("0." + "10" * 15, Rational(2, 3).to_ds(30, 2))
 	end
 end
