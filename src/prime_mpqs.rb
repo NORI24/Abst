2e059ddc7d736e79b3303bf34936a37a2a64ed8c
miyamoto <miyamoto@ee56edd8-5187-8145-a53a-5b855f78f1fc>
2011-09-22 18:25:23 +0000

    git-svn-id: svn+ssh://arith-c.math.se.tmu.ac.jp/var/svn/repos/abst/trunk@160 ee56edd8-5187-8145-a53a-5b855f78f1fc

--- src/prime_mpqs.rb
+++ src/prime_mpqs.rb
@@ -14,20 +14,115 @@ def mpqs(n, factor_base_size = nil, sieve_range = nil)
 		i += 1
 	end
 
-	sqrt_list = factor_base.map{|p| mod_sqrt(n, p)}
-
 	# Sieve
-	sieve = Array.new(sieve_range << 1)
 	sqrt = isqrt(n)
 	sqrt_half = sqrt >> 1
 	sieve_range = sqrt_half if sqrt_half < sieve_range
-	((sqrt - sieve_range)...(sqrt + sieve_range)).each.with_index do |r, i|
-		t = (r ** 2 - n)
-		p t
-		sieve[i] = Math.log(t.abs)
+	sieve = Array.new(sieve_range << 1)
+	lo = sqrt - sieve_range
+	hi = sqrt + sieve_range
+	(lo...hi).each.with_index do |r, i|
+		t = r ** 2 - n
+		factorization = Array.new(factor_base_size, 0)
+		sieve[i] = [r, t.abs, factorization]
+	end
+
+	# -1
+	(0..sieve_range).each do |i|
+		sieve[i][2][0] = 1
+	end
+
+	# 2
+	s = sieve[0][1].even? ? 0 : 1
+	s.step((sieve_range << 1) - 1, 2) do |i|
+		count = 1
+		count += 1 while sieve[i][1][count] == 0
+		sieve[i][2][1] = count
+		sieve[i][1] >>= count
+	end
+
+	# 3, ...
+	(2...factor_base.size).each do |i|
+		p = factor_base[i]
+		(1..5).each do |e|
+			sqrt = mod_sqrt(n, p, e)
+			[sqrt, p ** e - sqrt].each do |t|
+				s = (t - lo) % p ** e
+				s.step((sieve_range << 1) - 1, p ** e) do |j|
+					sieve[j][2][i] += 1
+					sieve[j][1] /= p
+				end
+			end
+		end
 	end
 
 	# Gaussian elimination
+	sieve = sieve.select{|i| 1 == i[1]}
+	factorization = sieve.map(&:last).map(&:dup)
+	rslt = gaussian_elimination(factorization)
 
-	return factor_base, sqrt_list
+	rslt.each do |row|
+		x = 1
+		f = Array.new(factor_base_size, 0)
+		row.each.with_index do |b, i|
+			next if b == 0
+			x = x * sieve[i][0] % n
+			f = f.zip(sieve[i][2]).map{|a, b| a + b}
+		end
+
+		y = 1
+		factor_base_size.times do |i|
+			y = y * power(factor_base[i], f[i] >> 1, n) % n
+		end
+
+		z = gcd(x - y, n)
+		p z
+
+		return z if 1 < z and z < n
+	end
+end
+
+def gaussian_elimination(m)
+	m.map!{|row| row.map{|i| i[0]}.reverse}
+
+	rslt = Array.new(m.size)
+	m.size.times do |i|
+		rslt[i] = Array.new(m.size, 0)
+		rslt[i][i] = 1
+	end
+
+	height = m.size
+	width = m[0].size
+
+	width.times do |j|
+		# Find non-zero entry
+		row = nil
+		(j...height).each do |i|
+			if 0 != m[i][j]
+				row = i
+				break
+			end
+		end
+		next unless row
+
+		# Swap?
+		if j < row
+			m[row], m[j] = m[j], m[row]
+			rslt[row], rslt[j] = rslt[j], rslt[row]
+		end
+
+		# Eliminate
+		((j + 1)...height).each do |i|
+			next if m[i][j] == 0
+
+			((j + 1)...width).each do |j2|
+				m[i][j2] ^= m[j][j2]
+			end
+			height.times do |j2|
+				rslt[i][j2] ^= rslt[j][j2]
+			end
+		end
+	end
+
+	return rslt.pop(height - width)
 end
