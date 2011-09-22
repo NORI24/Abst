dd601eba79e8eacd0dffefa2bb82e76d8d11af9e
miyamoto <miyamoto@ee56edd8-5187-8145-a53a-5b855f78f1fc>
2011-09-22 16:51:28 +0000

    add test case
    git-svn-id: svn+ssh://arith-c.math.se.tmu.ac.jp/var/svn/repos/abst/trunk@159 ee56edd8-5187-8145-a53a-5b855f78f1fc

---
+++ src/test/test_matrix.rb
@@ -0,0 +1,37 @@
+require 'test/unit'
+require 'abst'
+
+class TC_Matrix < Test::Unit::TestCase
+	def test_Matrix
+		matrix = Matrix(Rational, 2, 2)
+		assert_equal("Rational", matrix.coef_class.name)
+		assert_equal(2, matrix.height)
+		assert_equal(2, matrix.width)
+
+		m = Matrix(Rational, [[3, 5], [9, 2]])
+		assert_equal([[3, 5], [9, 2]], m)
+	end
+
+	def test_add
+		matrix = Matrix(Rational, 2, 2)
+		m1 = matrix.new([[2, 3], [4, 5]])
+		m2 = matrix.new([[3, 1], [2, -3]])
+
+		assert_equal([[5, 4], [6, 2]], m1 + m2)
+	end
+
+	def test_sub
+		matrix = Matrix(Rational, 2, 2)
+		m1 = matrix.new([[2, 3], [4, 5]])
+		m2 = matrix.new([[3, 1], [2, -3]])
+
+		assert_equal([[-1, 2], [2, 8]], m1 - m2)
+	end
+
+	def test_trace
+		matrix = Matrix(Rational, 2, 2)
+		m = matrix.new([[2, 3], [4, 5]])
+
+		assert_equal(7, m.trace)
+	end
+end
