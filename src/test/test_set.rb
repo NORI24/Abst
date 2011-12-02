d5053260c3e9856252035251be274ba5c0f6deb6
miyamoto <miyamoto@ee56edd8-5187-8145-a53a-5b855f78f1fc>
2011-12-02 17:36:28 +0000

    add test case
    git-svn-id: svn+ssh://arith-c.math.se.tmu.ac.jp/var/svn/repos/abst/trunk@257 ee56edd8-5187-8145-a53a-5b855f78f1fc

--- src/test/test_set.rb
+++ src/test/test_set.rb
@@ -3,14 +3,33 @@ require 'abst'
 
 class TC_Set < Test::Unit::TestCase
 	def test_add
-		# sortable
-		assert_equal([1, 2, 3].to_set, [1, 3].to_set.add(2))
-		assert_equal([2, 1, 2, 3].to_set, [1, 2, 3].to_set.add(2))
-		assert_equal([3, 1, 2, 3, 5].to_set, [5, 2, 3, 1].to_set.add(2))
+		assert_equal(Set.new([1, 2, 3]), Set.new([1, 3]).add(2))
+		assert_equal(Set.new([2, 1, 2, 3]), Set.new([1, 2, 3]).add(2))
+		assert_equal(Set.new([3, 1, 2, 3, 5]), Set.new([5, 2, 3, 1]).add(2))
+	end
 
-		# unsortable
-		assert_equal([1, 2, 3].to_set(false), [1, 3].to_set.add(2))
-		assert_equal([2, 1, 2, 3].to_set(false), [1, 2, 3].to_set.add(2))
-		assert_equal([3, 1, 2, 3, 5].to_set, [5, 2, 3, 1].to_set(false).add(2))
+	def test_dup
+		s1 = Set.new([1, 2, 3])
+		s2 = s1.dup
+		s2.add(4)
+
+		assert_equal(Set.new([1, 2, 3]), s1)
+	end
+end
+
+class TC_SortableSet < Test::Unit::TestCase
+	def test_add
+		assert_equal(SortableSet.new([1, 2, 3]), SortableSet.new([1, 3]).add(2))
+		assert_equal(SortableSet.new([2, 1, 2, 3]), SortableSet.new([1, 2, 3]).add(2))
+		assert_equal(SortableSet.new([3, 1, 2, 3, 5]), SortableSet.new([5, 2, 3, 1]).add(2))
+	end
+
+	def test_dup
+		s1 = SortableSet.new([1, 2, 3]){|a, b| b <=> a}
+		s2 = s1.dup
+		assert_equal(s1.to_a, s2.to_a)
+
+		s2.add(4)
+		assert_equal(Set.new([1, 2, 3]), s1)
 	end
 end
