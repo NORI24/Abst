0bff5095b97170fcb850dfdf8fe45e4d1d5dc83f
miyamoto <miyamoto@ee56edd8-5187-8145-a53a-5b855f78f1fc>
2011-12-03 10:52:00 +0000

    change interface
    git-svn-id: svn+ssh://arith-c.math.se.tmu.ac.jp/var/svn/repos/abst/trunk@259 ee56edd8-5187-8145-a53a-5b855f78f1fc

--- src/cache.rb
+++ src/cache.rb
@@ -1,32 +1,32 @@
 class Cache
 	@@prefix = 'abst_cache_'
 
-	def self.save(cache_name, data)
-		path = get_path(cache_name)
+	def self.[]=(cache_id, data)
+		path = get_path(cache_id)
 
 		Dir::mkdir(CACHE_DIR) unless FileTest.exist?(CACHE_DIR)
 		open(path, "w") {|io| Marshal.dump(data, io)}
 	end
 
-	def self.load(cache_name)
-		path = get_path(cache_name)
+	def self.[](cache_id)
+		path = get_path(cache_id)
 
 		if FileTest.exist?(path)
 			open(path) {|io| return Marshal.load(io)}
 		end
 
 		unless block_given?
-			raise RuntimeError, "There is no cache data '#{cache_name}'"
+			raise RuntimeError, "There is no cache data '#{cache_id}'"
 		end
 
 		data = yield
-		save(cache_name, data)
+		save(cache_id, data)
 
 		return data
 	end
 
-	def self.delete(cache_name)
-		path = get_path(cache_name)
+	def self.delete(cache_id)
+		path = get_path(cache_id)
 
 		File.delete(path) if FileTest.exist?(path)
 	end
@@ -35,13 +35,13 @@ class Cache
 		raise NotImplementedError
 	end
 
-	def self.exist?(cache_name)
-		return FileTest.exist?(get_path(cache_name))
+	def self.exist?(cache_id)
+		return FileTest.exist?(get_path(cache_id))
 	end
 
-	def self.get_path(cache_name)
-		raise ArgumentError, "Invalid cache_name. (a-z 0-9 and _ are available)" unless /[a-z0-9_]+/ =~ cache_name
+	def self.get_path(cache_id)
+		raise ArgumentError, "Invalid cache_id. (a-z 0-9 and _ are available)" unless /[a-z0-9_]+/ =~ cache_id
 
-		return CACHE_DIR + @@prefix + cache_name
+		return CACHE_DIR + @@prefix + cache_id
 	end
 end
