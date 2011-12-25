f677c66b32fb517b5825a6ed0da486c86a138cfb
miyamoto <miyamoto@ee56edd8-5187-8145-a53a-5b855f78f1fc>
2011-12-25 12:17:46 +0000

    git-svn-id: svn+ssh://arith-c.math.se.tmu.ac.jp/var/svn/repos/abst/trunk@275 ee56edd8-5187-8145-a53a-5b855f78f1fc

--- src/ant.rb
+++ src/ant.rb
@@ -1,27 +1,27 @@
 ABST_ROOT = File.dirname(__FILE__) + '/'
 
-require ABST_ROOT + 'compatibility'
+require_relative 'compatibility'
 
 # Integer block byte size
 BASE_BYTE = 1.size
 INFINITY = Float::INFINITY
 
-require ABST_ROOT + 'config'
+require_relative 'config'
 
-require ABST_ROOT + 'bisect'
-require ABST_ROOT + 'cache'
+require_relative 'bisect'
+require_relative 'cache'
 
-require ABST_ROOT + 'array'
-require ABST_ROOT + 'combination'
-require ABST_ROOT + 'complex'
-require ABST_ROOT + 'float'
-require ABST_ROOT + 'fundamental'
-require ABST_ROOT + 'integer'
-require ABST_ROOT + 'polynomial'
-require ABST_ROOT + 'prime'
-require ABST_ROOT + 'prime_mpqs'
-require ABST_ROOT + 'rational'
-require ABST_ROOT + 'sequence'
-require ABST_ROOT + 'set'
-require ABST_ROOT + 'vector'
-require ABST_ROOT + 'matrix'
+require_relative 'array'
+require_relative 'combination'
+require_relative 'complex'
+require_relative 'float'
+require_relative 'fundamental'
+require_relative 'integer'
+require_relative 'polynomial'
+require_relative 'prime'
+require_relative 'prime_mpqs'
+require_relative 'rational'
+require_relative 'sequence'
+require_relative 'set'
+require_relative 'vector'
+require_relative 'matrix'
