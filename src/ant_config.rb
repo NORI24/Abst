a5695c6353d5d30cb7f22b52de2be13112fa498d
miyamoto <miyamoto@ee56edd8-5187-8145-a53a-5b855f78f1fc>
2011-12-27 08:59:27 +0000

    git-svn-id: svn+ssh://arith-c.math.se.tmu.ac.jp/var/svn/repos/abst/trunk@283 ee56edd8-5187-8145-a53a-5b855f78f1fc

--- src/ant_config.rb
+++ src/ant_config.rb
@@ -1,17 +1,19 @@
-# Precompute primes under the value
-PRIME_CACHE_LIMIT = 1_000_000
+module ANT
+	# Precompute primes under the value
+	PRIME_CACHE_LIMIT = 1_000_000
 
-# System cache files will be created in this directory
-DATA_DIR = ANT_ROOT + 'data/'
+	# System cache files will be created in this directory
+	DATA_DIR = ANT_ROOT + 'data/'
 
-# User cache files will be created in this directory
-CACHE_DIR = ANT_ROOT + 'cache/'
+	# User cache files will be created in this directory
+	CACHE_DIR = ANT_ROOT + 'cache/'
 
-# Cache primes
-PRIMES_LIST = DATA_DIR + 'primes_list'
+	# Cache primes
+	PRIMES_LIST = DATA_DIR + 'primes_list'
 
-# Bit size of Fixnum
-# integer e s.t. (2 ** e) is not Fixnum and (2 ** e - 1) is Fixnum
-FIXNUM_BIT_SIZE = 30
+	# Bit size of Fixnum
+	# integer e s.t. (2 ** e) is not Fixnum and (2 ** e - 1) is Fixnum
+	FIXNUM_BIT_SIZE = 30
 
-MAX_THREAD = 1
+	MAX_THREAD = 1
+end
