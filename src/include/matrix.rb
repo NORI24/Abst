9f8ff4162974f5ccdf1c6ecdccd740528e5de323
miyamoto <miyamoto@ee56edd8-5187-8145-a53a-5b855f78f1fc>
2012-02-03 16:03:32 +0000

    git-svn-id: svn+ssh://arith-c.math.se.tmu.ac.jp/var/svn/repos/abst/trunk@358 ee56edd8-5187-8145-a53a-5b855f78f1fc

--- src/include/matrix.rb
+++ src/include/matrix.rb
@@ -42,7 +42,55 @@ module ANT
 			return self.class.new(@coef.zip(other.coef).map{|a, b| a.__send__(op, b)})
 		end
 
+		# Param::  self must be n * (n + 1) matrix s.t. (MB)
+		#          M is invertible n * n matrix
+		#          B is a column vector
+		# Return:: a column vector X s.t. MX == B
 		def solve
+			raise MatrixSizeError unless @height + 1 == @width
+
+			inverse = []
+			m = self.to_a
+			n = @height
+
+			n.times do |j|
+				# Find non-zero entry
+				row = nil
+				j.upto(n - 1) do |i|
+					unless m[i][j].zero?
+						row = i
+						break
+					end
+				end
+				return nil unless row
+
+				# Swap?
+				if j < row
+					m[row], m[j] = m[j], m[row]
+				end
+
+				# Eliminate
+				inverse[j] = m[j][j].inverse
+				(j + 1).upto(n - 1) do |i|
+					c = m[i][j] * inverse[j]
+
+					(j + 1).upto(n) do |j2|
+						m[i][j2] -= m[j][j2] * c
+					end
+				end
+			end
+
+			# Solve triangular system
+			x = []
+			(n - 1).downto(0) do |i|
+				temp = m[i][n]
+				(i + 1).upto(n - 1) do |j|
+					temp -= m[i][j] * x[j]
+				end
+				x[i] = temp * inverse[i]
+			end
+
+			return Vector(self.class.coef_class, x)
 		end
 
 		def to_a
@@ -50,7 +98,7 @@ module ANT
 		end
 
 		def to_s
-			return @coef.inspect
+			return @coef.to_s
 		end
 
 		def inspect
