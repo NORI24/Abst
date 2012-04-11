e3b201af685a7f9c0668d0707ae469fe701d06c1
abst <abst@ee56edd8-5187-8145-a53a-5b855f78f1fc>
2012-04-11 07:38:54 +0000

    add rails files
    git-svn-id: svn+ssh://arith-c.math.se.tmu.ac.jp/var/svn/repos/abst/trunk@404 ee56edd8-5187-8145-a53a-5b855f78f1fc

---
+++ manual/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Manual::Application.config.secret_token = 'c6f8c7bf27d16c6cf6cd43980f7516e046dceb3b58e19034926b673f03ada85205a8d8ec86f66c34f3f4727a44679bded13aa92860c12f76b5991932bdd9ca5d'
