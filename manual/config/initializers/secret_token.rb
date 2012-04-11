cef249be30455539eb6777cd01c8180668a6b6a5
abst <abst@ee56edd8-5187-8145-a53a-5b855f78f1fc>
2012-04-11 12:04:57 +0000

    git-svn-id: svn+ssh://arith-c.math.se.tmu.ac.jp/var/svn/repos/abst/trunk@408 ee56edd8-5187-8145-a53a-5b855f78f1fc

---
+++ manual/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Manual::Application.config.secret_token = 'b1b94ce09adabc0d40d92475032a432269c6a2a08b152398a83e5124faaad0cba6eb89ab89d6bd2d50a113e33c49ee9fff5a8b962b79c1fc11de7d206ae5f0b1'
