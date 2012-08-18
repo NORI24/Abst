0ad16885aa45ae4325aa4781eaec5e1cc0eb30e8
miyamoto <miyamoto@ee56edd8-5187-8145-a53a-5b855f78f1fc>
2012-08-18 20:41:19 +0000

    git-svn-id: svn+ssh://arith-c.math.se.tmu.ac.jp/var/svn/repos/abst/trunk@419 ee56edd8-5187-8145-a53a-5b855f78f1fc

--- lib/include/enumerable.rb
+++ lib/include/enumerable.rb
@@ -1,11 +1,13 @@
 module Enumerable
 	def sum
+		return self.inject(&:+) unless block_given?
 		rslt = nil
 		self.each{|v| rslt ? rslt += yield(v) : rslt = yield(v)}
 		return rslt
 	end
 
 	def prod
+		return self.inject(&:*) unless block_given?
 		rslt = nil
 		self.each{|v| rslt ? rslt *= yield(v) : rslt = yield(v)}
 		return rslt
