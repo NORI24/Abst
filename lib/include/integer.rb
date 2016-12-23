5eb50a463d6181974ca8006442ff902f94f3d55e
Yasunori Miyamoto <m.yasunori.eip@gmail.com>
2016-12-23 14:25:15 +0900

    平方数チェック用の配列の定義をメソッドの外へ移動し、処理を効率化

--- lib/include/integer.rb
+++ lib/include/integer.rb
@@ -126,24 +126,24 @@ class Integer
 		return (r - 1) >> 1
 	end
 
+	@@square_check_array = {
+		11=>[true, true, false, true, true, true, false, false, false, true, false],
+		63=>[true, true, false, false, true, false, false, true, false, true, false, false, false, false, false, false, true, false, true, false, false, false, true, false, false, true, false, false, true, false, false, false, false, false, false, false, true, true, false, false, false, false, false, true, false, false, true, false, false, true, false, false, false, false, false, false, false, false, true,false, false, false, false],
+		64=>[true, true, false, false, true, false, false, false, false, true, false, false, false, false, false, false, true, true, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, true, false, false, true, false, false, false, false, true, false, false, false, false, false, false,false, true, false, false, false, false, false, false, false, true, false, false, false, false, false, false],
+		65=>[true, true, false, false, true, false, false, false, false, true, true, false, false, false, true, false, true, false, false, false, false, false, false, false, false, true, true, false, false, true, true, false, false, false, false, true, true, false, false, true, true, false, false, false, false, false, false, false, false, true, false, true, false, false, false, true, true, false, false, false, false, true, false, false, true]
+	}
+
 	# Test whether self is a square number or not
 	# Param::  positive integer self
 	# Return:: square root of self if self is square else false
 	def square?
-		check = {
-			11=>[true, true, false, true, true, true, false, false, false, true, false],
-			63=>[true, true, false, false, true, false, false, true, false, true, false, false, false, false, false, false, true, false, true, false, false, false, true, false, false, true, false, false, true, false, false, false, false, false, false, false, true, true, false, false, false, false, false, true, false, false, true, false, false, true, false, false, false, false, false, false, false, false, true,false, false, false, false],
-			64=>[true, true, false, false, true, false, false, false, false, true, false, false, false, false, false, false, true, true, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, true, false, false, true, false, false, false, false, true, false, false, false, false, false, false,false, true, false, false, false, false, false, false, false, true, false, false, false, false, false, false],
-			65=>[true, true, false, false, true, false, false, false, false, true, true, false, false, false, true, false, true, false, false, false, false, false, false, false, false, true, true, false, false, true, true, false, false, false, false, true, true, false, false, true, true, false, false, false, false, false, false, false, false, true, false, true, false, false, false, true, true, false, false, false, false, true, false, false, true]
-		}
-
 		# 64
 		t = self & 63
-		return false unless check[64][t]
+		return false unless @@square_check_array[64][t]
 
 		r = self % 45045	# == 63 * 65 * 11
 		[63, 65, 11].each do |c|
-			return false unless check[c][r % c]
+			return false unless @@square_check_array[c][r % c]
 		end
 
 		q = Abst.isqrt(self)
